330d72c9764072ae21f8a8941975f18ee11708e5
Bingdom <bingdom1@gmail.com>
2025-04-07 10:40:13 +1000

    feat: added tests

--- nuitka_reporter/__init__.py
+++ nuitka_reporter/__init__.py
@@ -1,3 +1,5 @@
 from .main import to_html
 
+__version__ = "0.1.0"
+
 __all__ = ["to_html"]
---
+++ tests/test_generate.py
@@ -0,0 +1,14 @@
+from nuitka_reporter import to_html
+import os
+
+
+def test_generate():
+    current_dir = os.path.dirname(__file__)
+    filename = os.path.join(current_dir, "data/compilation-report.xml")
+    output_filename = to_html(os.path.join(filename, current_dir))
+
+    assert filename == output_filename, "Input filename must match with output filename"
+
+    assert os.path.isfile(output_filename), "Output file must exist"
+
+    assert os.path.getsize(output_filename) > 0, "Output file is 0 kb"
